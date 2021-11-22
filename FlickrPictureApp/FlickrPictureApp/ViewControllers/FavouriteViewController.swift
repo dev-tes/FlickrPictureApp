531291a0fe5a7b3c862c0dcb00d9e3c66ccbf831
dev-tes <tesleem.amuda@gmail.com>
2021-11-22 18:18:24 +0100

    create viewControllers

--- FlickrPictureApp/FlickrPictureApp/ViewControllers/FavouriteViewController.swift
+++ FlickrPictureApp/FlickrPictureApp/ViewControllers/FavouriteViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavouriteViewController.swift
 //  FlickrPictureApp
 //
 //  Created by  Decagon on 22/11/2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavouriteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
     }
 
-
 }
-
---
+++ FlickrPictureApp/FlickrPictureApp/ViewControllers/PhotosViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FlickrPictureApp
+//
+//  Created by  Decagon on 22/11/2021.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
+
